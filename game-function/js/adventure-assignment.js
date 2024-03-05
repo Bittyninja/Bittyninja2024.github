0bd4318713478249d70513abc5c7ff77e9e1dd0e
Bittyninja <156446312+Bittyninja@users.noreply.github.com>
2024-03-05 12:50:55 -0500

    adventure-assignment

--- game-function/js/adventure-assignment.js
+++ game-function/js/adventure-assignment.js
@@ -46,37 +46,46 @@ function reactToMoveRightButton() {
 }
 
 function reactToMoveLeftButton() {
+    moveLeft();
 
 }
 
 function reactToMoveDownButton() {
+    moveDown();
 
 }
 
 function reactToMoveUpButton() {
+    moveUp();
 
 }
 
 function reactToAttackButton() {
+    attack();
 
 }
 
 function reactToJumpRightButton() {
+    jumpRight();
 
 }
 
 function reactToJumpLeftButton() {
+    jumpLeft();
 
 }
 
 function reactToJumpDownButton() {
+    jumpDown();
 
 }
 
 function reactToJumpUpButton() {
+    jumpUp();
 
 }
 
 function reactToJumpButton() {
+    jump();
 
 }
